158e45e4d2981cab59849387fe7144fe8163fa7e
ravi11-developer <ravisahu4615@gmail.com>
2025-11-22 13:58:26 +0530

    first

---
+++ zerp.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+running=True
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+screen=pygame.display.set_mode((640,240))
+while running:
+    for event in pygame.event.get():
+        print(event)
+        if event.type==pygame.QUIT:
+            running=False
+    screen.fill(YELLOW)
+    pygame.display.update() 
+
+        
+pygame.quit()
