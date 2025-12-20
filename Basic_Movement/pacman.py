0c93819ec8d4be6225d592bdd741df53d6bc1258
ravi11-developer <ravisahu4615@gmail.com>
2025-12-20 18:48:50 +0530

    opdatemovement

--- Basic_Movement/pacman.py
+++ Basic_Movement/pacman.py
@@ -15,6 +15,7 @@ class Pacman(object):
         self.color = YELLOW
         self.node=node
         self.setPosition()
+        self.target=node
 
     def setPosition(self):
         self.position=self.node.position.copy()
@@ -22,11 +23,16 @@ class Pacman(object):
 
 
     def update(self, dt):	
-        # self.position += self.directions[self.direction]*self.speed*dt
+        self.position += self.directions[self.direction]*self.speed*dt
         direction = self.getValidKey()
-        self.direction = direction
-        self.node=self.getNewTarget(direction)
-        self.setPosition()
+        if self.overshotTarget():
+            self.node=self.target
+            self.target=self.getNewTarget(direction)
+            if self.target is not self.node:
+                self.direction=direction
+            else :
+                self.direction=STOP
+            self.setPosition()
 
     def validDirection(self,direction):
         if direction is not STOP:
@@ -42,7 +48,7 @@ class Pacman(object):
     def getValidKey(self):
         key_pressed = pygame.key.get_pressed()
         if key_pressed[K_UP]:
-            return UP
+            return UP 
         if key_pressed[K_DOWN]:
             return DOWN
         if key_pressed[K_LEFT]:
@@ -54,3 +60,14 @@ class Pacman(object):
     def render(self, screen):
         p = self.position.asInt()
         pygame.draw.circle(screen, self.color, p, self.radius)
+
+    
+    def overshotTarget(self):
+        if self.target is not None:
+            vec1=self.target.position-self.node.position
+            vec2=self.position-self.node.position
+            node2target=vec1.magnitudeSquared()
+            node2Self=vec2.magnitudeSquared()
+            return node2Self>=node2target
+        return False
+    
